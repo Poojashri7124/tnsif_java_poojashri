e1bd95189b0b4f58f6201ae1f799314ba4ec533a
Poojashri7124 <poojashrikumaraguru@gmail.com>
2025-09-29 23:14:20 +0530

    Implemented encapsulation (OOPs concept) in Java Program

---
+++ tnsif_java_poojashri/src/oop_encapsulation/EncapsulationDemo.java
@@ -0,0 +1,15 @@
+package oop_encapsulation;
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		Encapsulation obj = new Encapsulation();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
+}
+		
+
