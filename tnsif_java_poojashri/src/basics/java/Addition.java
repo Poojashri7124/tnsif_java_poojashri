25f69080aca9106bebda3b1bf862c122f8230249
Poojashri7124 <poojashrikumaraguru@gmail.com>
2025-09-30 20:20:21 +0530

    Basic Java programs to perform looping, conditional and data types.

---
+++ tnsif_java_poojashri/src/basics/java/Addition.java
@@ -0,0 +1,14 @@
+package basics.java;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a = Integer.parseInt(args[1]);
+        int b = Integer.parseInt(args[1]);
+        int c = a + b;
+        
+        System.out.println("Addition of a = " + a + ", b = " + b + " is " + c);
+
+	}
+
+}
---
+++ tnsif_java_poojashri/src/basics/java/IfElse.java
@@ -0,0 +1,15 @@
+package basics.java;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+int age=11;
+		
+		if(age>=18)
+			System.out.println(" You are eligible for Voting");	
+		else
+			System.out.println("Sorry! You are not eligible");
+
+	}
+
+}
