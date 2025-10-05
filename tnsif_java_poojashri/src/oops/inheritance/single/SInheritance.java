a98b9a23fc3285bf29d23e90ee7a7892dd9f17d0
Poojashri7124 <poojashrikumaraguru@gmail.com>
2025-10-05 10:27:57 +0530

    Java Program to perform the single inheritance to get citizen details

---
+++ tnsif_java_poojashri/src/oops/inheritance/single/SInheritance.java
@@ -0,0 +1,11 @@
+package oops.inheritance.single;
+
+public class SInheritance {
+
+	public static void main(String[] args) {
+		Student student = new Student("Pooja", "8080908080", "Bangalore", 9847878787L, 10, "ABC College");
+		System.out.println(student);
+
+	}
+
+}
